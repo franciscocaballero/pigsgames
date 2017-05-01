313cc296e120a89ccf1375524bcd64b6052bd2dd
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-05-01 16:54:42 -0400

    dice images add/dice roll is now working

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-// gives random Whole number for 'dice'
-dice = Math.floor(Math.random()* 6) + 1;
-
 /// lets you a access the DOM and select a css element / gives you access to css text element
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
-var x = document.querySelector('#score-0').textContent;
-
+//var x = document.querySelector('#score-0').textContent;
+/// DOM acess/css acess
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// roll button selceted and event listenr 'click' is added
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  // 1. Generates Random number and also cahnges decimal to whole number
+var dice = Math.floor(Math.random()* 6) + 1;
+
+//2. Display result
+var diceDOM = document.querySelector('.dice')
+diceDOM.style.display = 'block';
+diceDOM.src = 'images/dice-' + dice + '.png';
+
+//3. update roundScore IF number was Not a 1
+
+});
