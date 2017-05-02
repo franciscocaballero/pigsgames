618ea8f6ebcd741bb8f7c4ea0ca39c3dcab2c568
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-05-02 00:15:04 -0400

    added more to init function

--- app.js
+++ app.js
@@ -2,31 +2,16 @@
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-/// lets you a access the DOM and select a css element / gives you access to css text element
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-//var x = document.querySelector('#score-0').textContent;
-/// DOM acess/css acess
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 // roll button selceted and event listenr 'click' is added
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
   // 1. Generates Random number and also cahnges decimal to whole number
-var dice = Math.floor(Math.random()* 6) + 1;
+var dice = Math.floor(Math.random() * 6) + 1;
 
 //2. Display result
-var diceDOM = document.querySelector('.dice')
+var diceDOM = document.querySelector('.dice');
 diceDOM.style.display = 'block';
 diceDOM.src = 'images/dice-' + dice + '.png';
 
@@ -51,16 +36,16 @@ scores[activePlayer] += roundScore;
   // update the UI
 document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
   //check if player has won game
-if (scores[activePlayer] >= 100) {
-  document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'
+if (scores[activePlayer] >= 10) {
+  document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
   document.querySelector('.dice').style.display = 'none';
-  document.querySelector('.player-1' + activeplayer + '-panel').classList.add('winner');
+  document.querySelector('.player-' + activeplayer + '-panel').classList.add('winner');
+  document.querySelector('.player-' + activeplayer + '-panel').classList.remove('active');
 } else {
 //next Player
   nextPlayer();
 
 }
-
 });
 
 function nextPlayer() {
@@ -70,9 +55,38 @@ function nextPlayer() {
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
 
-  document.querySelector('.player-0-panel').classList.toggle('active')
-  document.querySelector('.player-1-panel').classList.toggle('active')
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
 
   document.querySelector('.dice').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  /// lets you a access the DOM and select a css element / gives you access to css text element
+  //document.querySelector('#current-' + activePlayer).textContent = dice;
+  // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+
+  //var x = document.querySelector('#score-0').textContent;
+  /// DOM acess/css acess
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
 
 }
